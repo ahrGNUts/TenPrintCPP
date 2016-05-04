5c6017948d25a9e62b215b43016f90fe82789908
Patrick <revmydev@gmail.com>
2016-05-04 12:59:53 -0700

    Initial commit Program not originally tracked with VCS. Essentially complete.

---
+++ tenPrint/main.cpp
@@ -0,0 +1,28 @@
+// this is a C++ translation of the old 10 PRINT C64 BASIC program
+#include <iostream>
+#include <ctime>
+#include <conio.h>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+	srand((unsigned)time(0));
+	int charType;
+
+	while(1)
+	{
+		charType = 0 + rand() % (1 - 0 + 1);
+
+		// ascii implementation
+		if(charType == 0)
+			cout << "\\";
+		else
+			cout << '/';
+			
+
+		Sleep(1);// you need Windows.h for this one
+	}// end while
+
+	_getch();
+}
